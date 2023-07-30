e48fdd2831c6241ff08e572b0bf4e4deb6999d2a
Anil_Soni007 <115683735+anilsoni007@users.noreply.github.com>
2023-07-30 16:52:57 +0530

    Create delete_vol_basedon_tags.py

---
+++ delete_vol_basedon_tags.py
@@ -0,0 +1,7 @@
+import boto3
+aws_mang_cons=boto3.session.Session(profile_name='ec2_wala')
+vols_cons=aws_mang_cons.client(service_name='ec2',region_name='ap-south-1')
+response=vols_cons.describe_volumes(Filters=[{'Name': 'tag:Env','Values': ['Dev']}]) # to get vol id which has key Env and value as Dev.
+for each in response.get('Volumes'):
+    #print(each.get('VolumeId'))
+    vols_cons.delete_volume(VolumeId=each.get('VolumeId'))
